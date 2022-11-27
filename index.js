bfc99f29c7405daccd6fa8fc0a3279c7f6303c56
mosesochacha <ochachamoses443@gmail.com>
2022-11-27 11:13:27 +0300

    complited assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(spy){
+    return spy()
+}
+receivesAFunction();
+function returnsANamedFunction(){
+    return function drinking(){  
+    }
+    return returnsANamedFunction
+}
+function returnsAnAnonymousFunction(){
+    return()=>{
+
+    }
+}
+returnsAnAnonymousFunction()
